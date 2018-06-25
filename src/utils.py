0536b9cfda22e75f7daab1e982a1d6033b86e894
pomelyu <pomelyu5199@icloud.com>
2018-06-25 18:14:46 +0800

    :art: add util to load label and load image with PIL

--- src/utils.py
+++ src/utils.py
@@ -1,8 +1,44 @@
 import matplotlib.pyplot as plt
 import matplotlib.patches as patches
 import os
+from PIL import Image
 
 from .constants import OBJECT_CLASS
+
+def load_image(image_path):
+    """ load image from file
+    Arguments:
+    image_path: relative path of the image
+
+    Return:
+    image: PIL image object
+    """
+    cwd = os.getcwd()
+    img = Image.open(os.path.join(cwd, image_path))
+
+    return img
+
+
+def load_labels(label_path):
+    """ load label information from file
+    Arguments:
+    label_path: relative path of the file preprcossed by darknet tool
+        line example: classname x y w h (x, y, w, h is normalized)
+
+    Return:
+    labels: array of object information
+        [[class_label, x, y, w, h], [class_label, x, y, w, h]]
+    """
+    cwd = os.getcwd()
+
+    labels = []
+    with open(os.path.join(cwd, label_path), 'r') as fin:
+        for line in fin:
+            obj = [ float(num) for num in line.split(" ") ]
+            obj[0] = int(obj[0])
+            labels.append(obj)
+
+    return labels
 
 def draw_label_rect(name, cx, cy, w, h, ax, color="blue"):
     x = cx - (w / 2)
@@ -12,7 +48,7 @@ def draw_label_rect(name, cx, cy, w, h, ax, color="blue"):
     ax.add_patch(patches.Rectangle((x, y), w, h, fill=False, linewidth=1, edgecolor=color))
 
 
-def visualize_label(img_path, label_path):
+def visualize_label(image_path, label_path):
     """ display the bounding box and classes of objects in the image
 
     Arguments:
@@ -24,20 +60,17 @@ def visualize_label(img_path, label_path):
     visualize_label("../data/VOC2007/images/000007.jpg", "../data/VOC2007/labels/000007.txt")
 
     """
-
-    cwd = os.getcwd()
-    img = plt.imread(os.path.join(cwd, img_path))
+    img = load_image(image_path)
+    labels = load_labels(label_path)
     
-    plt.figure(figsize=(10, 5))
+    plt.figure(figsize=(10, 10))
     ax = plt.gca()
-    ax.set_title(img_path)
+    ax.set_title(image_path)
     ax.imshow(img)
     
-    h, w, _ = img.shape
-    with open(os.path.join(cwd, label_path), 'r') as fin:
-        for line in fin:
-            obj = [ float(num) for num in line.split(" ") ]
-            class_name = OBJECT_CLASS[int(obj[0])]
-            draw_label_rect(class_name, obj[1] * w, obj[2] * h, obj[3] * w, obj[4] * h, ax)
+    w, h = img.size
+    for label in labels:
+        class_name = OBJECT_CLASS[label[0]]
+        draw_label_rect(class_name, label[1] * w, label[2] * h, label[3] * w, label[4] * h, ax)
 
     plt.show()
