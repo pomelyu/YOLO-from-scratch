ea9a2098740a05e13de5bdcd1c58b8b4d10c74ef
pomelyu <pomelyu5199@icloud.com>
2018-06-26 22:17:09 +0800

    :sparkles: generate batch with shuffle

--- src/training.py
+++ src/training.py
@@ -4,22 +4,25 @@ import numpy as np
 from .constants import GRID_SIZE, NUM_BOX
 from .utils import load_image, isExtension
 
-def batch_generator(imgs_path, labels_path, batch_size=32):
+def batch_generator(imgs_path, labels_path, batch_size=32, random_seed=0):
     imgs = sorted([file for file in os.listdir(imgs_path) if isExtension(file, ".jpg")])
     labels = sorted([label for label in os.listdir(labels_path) if isExtension(label, ".npy")])
     assert len(imgs) == len(labels)
     
     m = len(imgs)
-    for i in range(0, m, batch_size):
+    np.random.seed(random_seed)
+    indexs = np.random.permutation(m)
+    for offset in range(0, m, batch_size):
         X = []
         Y = []
-        for j in range(i, min(i+batch_size, m)):
-            assert imgs[j][:imgs[j].rindex(".")] == labels[j][:labels[j].rindex(".")]
+        batch_index = indexs[offset:min(offset+batch_size, m)]
+        for i in batch_index:
+            assert imgs[i][:imgs[i].rindex(".")] == labels[i][:labels[i].rindex(".")]
             
-            image = load_image(os.path.join(imgs_path, imgs[j]))
+            image = load_image(os.path.join(imgs_path, imgs[i]))
             X.append(np.asarray(image))
             
-            bboxs = np.load(os.path.join(labels_path, labels[j]))
+            bboxs = np.load(os.path.join(labels_path, labels[i]))
             bboxs = bboxs.reshape((GRID_SIZE, GRID_SIZE, NUM_BOX, -1))
             Y.append(bboxs)
             
