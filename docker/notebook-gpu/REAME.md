0bfc8ccc13bdab04f02c2979b4bd5540180d2aa4
pomelyu <pomelyu5199@icloud.com>
2018-06-27 18:24:11 +0800

    :whale::construction: replace docker build directory to tensor official repository

--- docker/notebook-gpu/REAME.md
+++
@@ -1,4 +0,0 @@
-Build the docker image with cuda support and basic library for tensor flow task
-
-Modified from [Official Dockerfile.gpu](https://github.com/tensorflow/tensorflow/tree/master/tensorflow/tools/docker)
-
--- docker/notebook-gpu/simple_console.py
+++ docker/notebook-gpu/simple_console.py
@@ -12,20 +12,22 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 # ==============================================================================
-import os
-from IPython.lib import passwd
 
-c = c  # pylint:disable=undefined-variable
-c.NotebookApp.ip = '*'
-c.NotebookApp.port = int(os.getenv('PORT', 8888))
-c.NotebookApp.open_browser = False
+"""Start a simple interactive console with TensorFlow available."""
 
-# sets a password if PASSWORD is set in the environment
-if 'PASSWORD' in os.environ:
-  password = os.environ['PASSWORD']
-  if password:
-    c.NotebookApp.password = passwd(password)
-  else:
-    c.NotebookApp.password = ''
-    c.NotebookApp.token = ''
-  del os.environ['PASSWORD']
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import code
+import sys
+
+
+def main(_):
+  """Run an interactive console."""
+  code.interact()
+  return 0
+
+
+if __name__ == '__main__':
+  sys.exit(main(sys.argv))
