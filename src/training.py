af802f965cee08ba1a92f67e2fbf6ce344d21069
pomelyu <pomelyu5199@icloud.com>
2018-08-28 11:08:38 +0800

    :hammer: apply resize transform to data_generator in training

--- src/training.py
+++ src/training.py
@@ -2,67 +2,68 @@ import os
 import numpy as np
 from keras.callbacks import ModelCheckpoint, TerminateOnNaN
 
-from .constants import GRID_SIZE, NUM_BOX, GAMMA_MIN, GAMMA_MAX
-from .io import load_image
-from .image_transform import flip_image_horizontal, adjust_gamma, covert_to_VGG_input
+from .constants import GRID_SIZE, NUM_BOX, GAMMA_MIN, GAMMA_MAX, MODEL_DIM
+from .io import load_image, load_label
+from .preprocess import ResizeTransform, bbox_2_bbox_map
+from .image_transform import covert_to_VGG_input
 
-        
-def generator_from_array(labels, images, batch_size=32, vgg_input=False, normalized=False):
-    assert len(labels) == len(images)
-    m = len(labels)
-    
+def data_generator(image_dir, label_dir, anchors, batch_size=32, shuffle=True, augment=False, 
+    vgg_input=False, normalized=False):
+
+    files = np.array([label.replace(".txt", "") for label in os.listdir(label_dir) if label.endswith(".txt")])
+    n = len(files)
+
+    resize_transform = ResizeTransform()
     while True:
-        index = np.random.permutation(m)
-        for offset in range(0, m, batch_size):
-            X = []
-            Y = []
-            for i in index[offset:offset+batch_size]:
-                image = load_image(images[i])
-                bboxs = np.load(labels[i])
-                    
-                # convert image to vgg format
+        if shuffle:
+            files = files[np.random.permutation(n)]
+
+        for offset in range(0, n, batch_size):
+            images = []
+            labels = []
+            for file_name in files[offset:offset+batch_size]:
+                image = load_image(os.path.join(image_dir, "{}.jpg".format(file_name)))
+                label = load_label(os.path.join(label_dir, "{}.txt".format(file_name)))
+
+                image, label[:, 1:] = resize_transform.transform(image, label[:, 1:])
+                label = bbox_2_bbox_map(label, anchors)
+
                 if vgg_input:
                     image = covert_to_VGG_input(image)
                 if normalized:
                     image = image / 255
 
-                X.append(np.asarray(image))
-                Y.append(bboxs)
-                
-            X = np.stack(X, axis=0)
-            Y = np.stack(Y, axis=0)
-            
-            yield X, Y
+                images.append(image)
+                labels.append(label)
 
-def train_valid_yolo(model, train_images_dir, train_labels_dir, valid_images_dir, valid_labels_dir,
+            yield np.stack(images, axis=0), np.stack(labels, axis=0)
+
+
+def train_valid_yolo(model, anchors, train_images_dir, train_labels_dir, valid_images_dir, valid_labels_dir,
     model_name=None, model_dir="./", batch_size=32, epochs=10, epoch_begin=0, 
-    vgg_input=False, normalized=False):
-
-    apply_labels_dir = np.vectorize(lambda label, labels_dir: os.path.join(labels_dir, label))
-    apply_images_dir = np.vectorize(lambda label, images_dir: os.path.join(images_dir, label.replace(".npy", ".jpg")))
+    augment=False, vgg_input=False, normalized=False):
     
     # Training Generator
-    train_labels = np.array([label for label in os.listdir(train_labels_dir) if label.endswith(".npy")])
-    train_images = apply_images_dir(train_labels, train_images_dir)
-    train_labels = apply_labels_dir(train_labels, train_labels_dir)
-    m_train = len(train_labels)
+    m_train = len([label for label in os.listdir(train_labels_dir) if label.endswith(".txt")])
     
-    train_generator = generator_from_array(
-        train_labels, 
-        train_images,
+    train_generator = data_generator(
+        train_images_dir, 
+        train_labels_dir,
+        anchors=anchors,
+        shuffle=True,
+        augment=augment,
         batch_size=batch_size, 
         vgg_input=vgg_input,
         normalized=normalized)
 
     # Validation Generator
-    valid_labels = np.array([label for label in os.listdir(valid_labels_dir) if label.endswith(".npy")])
-    valid_images = apply_images_dir(valid_labels, valid_images_dir)
-    valid_labels = apply_labels_dir(valid_labels, valid_labels_dir)
-    m_valid = len(valid_labels)
+    m_valid = len([label for label in os.listdir(valid_labels_dir) if label.endswith(".txt")])
 
-    valid_generator = generator_from_array(
-        valid_labels,
-        valid_images,
+    valid_generator = data_generator(
+        valid_images_dir,
+        valid_labels_dir,
+        anchors=anchors,
+        shuffle=False,
         batch_size=batch_size,
         vgg_input=vgg_input,
         normalized=normalized)
@@ -87,19 +88,15 @@ def train_valid_yolo(model, train_images_dir, train_labels_dir, valid_images_dir
 
         
 
-def evaluate_yolo(model, images_dir, labels_dir, batch_size=32, vgg_input=False, normalized=False):
-    test_labels = np.array([label for label in os.listdir(labels_dir) if label.endswith(".npy")])
+def evaluate_yolo(model, anchors, images_dir, labels_dir, batch_size=32, vgg_input=False, normalized=False):
+    test_labels = np.array([label for label in os.listdir(labels_dir) if label.endswith(".txt")])
     m = len(test_labels)
     
-    apply_labels_dir = np.vectorize(lambda label, labels_dir: os.path.join(labels_dir, label))
-    apply_images_dir = np.vectorize(lambda label, images_dir: os.path.join(images_dir, label.replace(".npy", ".jpg")))
-    
-    test_images = apply_images_dir(test_labels, images_dir)
-    test_labels = apply_labels_dir(test_labels, labels_dir)
-    
-    test_generator = generator_from_array(
-        test_labels,
-        test_images,
+    test_generator = data_generator(
+        images_dir,
+        labels_dir,
+        anchors=anchors,
+        shuffle=False,
         batch_size=batch_size,
         vgg_input=vgg_input,
         normalized=normalized)
